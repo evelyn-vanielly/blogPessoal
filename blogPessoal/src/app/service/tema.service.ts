789a40e142e4036413ed564f6e2a6a2a2e92b20e
evelyn.vanielly <evelyn.vanielly@gmail.com>
2020-09-01 22:41:58 -0300

    blog com os gets

---
+++ blogPessoal/src/app/service/tema.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', localStorage.getItem('token'))
+  }
+
+getAllTemas(){
+  return this.http.get('http://localhost:8080/tema', this.token)
+}
+getByIdTema(id:number){
+  return this.http.get(`http://localhost:8080/tema/${id}`, this.token)
+}
+
+}
